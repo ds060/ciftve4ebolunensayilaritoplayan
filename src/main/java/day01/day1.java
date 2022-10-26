2af14b5ea0219c4df6a5a611d148022aa955878d
ds06 <115063159+ds060@users.noreply.github.com>
2022-10-26 10:59:25 +0300

    vb

--- src/main/java/day01/day1.java
+++ src/main/java/day01/day1.java
@@ -1,2 +1,21 @@
-package day01;public class day1 {
+package day01;
+
+import java.util.Scanner;
+
+public class day1 {
+    public static void main(String[] args) {
+        int n;
+        int total=0;
+        Scanner scan = new Scanner(System.in);
+
+        do {System.out.println("sayı giriniz:");
+            n= scan.nextInt();
+            if ((n%2==0)&&(n%4==0)){
+                total+=n;
+
+
+            }
+        } while ((n!=1)&&(n%2==0));
+        System.out.println("toplam:" +total);
+    }
 }
