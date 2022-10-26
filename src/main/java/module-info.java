7fa9fe702622e2b2cb92853217faf26ca15db77d
ds06 <115063159+ds060@users.noreply.github.com>
2022-10-26 10:59:58 +0300

    vbb

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.batch {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.batch to javafx.fxml;
     exports com.example.batch;
 }
