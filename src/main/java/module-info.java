5f72f52e98f35b32486a140e2cd9352d6b62f8f9
ds06 <115063159+ds060@users.noreply.github.com>
2022-10-26 10:57:34 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.batch {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.batch to javafx.fxml;
+    exports com.example.batch;
+}
